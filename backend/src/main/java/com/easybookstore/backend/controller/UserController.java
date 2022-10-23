aff897caa798ac9aaaea7f982d983a7e915bebd4
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2022-10-23 11:09:23 -0500

    added cors setting in the backend

--- backend/src/main/java/com/easybookstore/backend/controller/UserController.java
+++ backend/src/main/java/com/easybookstore/backend/controller/UserController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,7 +24,7 @@ public class UserController {
 	@Autowired
 	UserService service;
 	
-	
+	@CrossOrigin(origins = "http://localhost:3000")
 	@GetMapping("/users")
 	public ResponseEntity<List<Users>> getUsers(){
 		List<Users> userList = new LinkedList<>();
