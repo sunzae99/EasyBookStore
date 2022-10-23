facf968d1a7bf2b07c10c3400f6ee42ec60ff351
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2022-10-23 10:54:44 -0500

    added cors setting in the backend and calling the api from the frontend

--- frontend/src/UseApiUpdate.js
+++ frontend/src/UseApiUpdate.js
@@ -1,30 +1,44 @@
 import React, { useState, useEffect } from "react";
 
-const url = "http://localhost:7777/easybook/v1/api/users";
+const url = "http://192.168.1.104:7777/easybook/v1/api/users";
 const UseApiUpdate = () => {
-	const [joke, setJoke] = useState("");
+  const [users, setUsers] = useState([]);
 
-	const getJoke = async () => {
-		const response = await fetch(url, {
-			method: "GET",
-			headers: {
-				"Content-Type": "text/plain",
-			},
-		});
-		const data = await response.json();
-		setJoke(data + "......");
-		console.log(data + "......");
-	};
+  const getUsers = async () => {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    });
+    const data = await response.json();
+    setUsers(data);
+    console.log(data);
+  };
 
-	useEffect(() => {
-		getJoke();
-	}, []);
+  useEffect(() => {
+    getUsers();
+  }, []);
 
-	return (
-		<>
-			<p>{joke}</p>
-		</>
-	);
+  return (
+    <>
+      <h1>Hello World</h1>
+      <ul>
+        {users.map((user) => {
+          const { userId, userName, email, address, password, phone } = user;
+          return (
+            <li key={userId}>
+              <h2>{userName}</h2>
+              <h4>{email}</h4>
+              <h4>{address}</h4>
+              <h4>{password}</h4>
+              <h4>{phone}</h4>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 
 export default UseApiUpdate;
